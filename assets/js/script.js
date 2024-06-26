4d235c86a281c89df96cd01896c3634dd85c9fe0
Navid bahadorani <naviid1989@gmail.com>
2024-06-26 11:15:42 +0000

    Set up structutr of game function and made add event listener for DOM and runGame, checkAnsweer, and calculateCorrectAnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,10 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons){
         button.addEventListener("click", function(){
             if(this.getAttribute("data-type") === "submit"){
-                alert("You clicked submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
-                 alert(`You clicked ${gameType}`);
+                 runGame(gameType);
             }
         })
         
@@ -26,4 +26,71 @@ document.addEventListener("DOMContentLoaded", function(){
 function runGame() {
     let num1 = Math.floor(Math.random() * 25)+1;
     let num2 = Math.floor(Math.random() * 25)+1;
+}
+
+/**
+ * Check the answer against the first element in
+ * the returned calculateCorrectAnswer array.
+ */
+
+function checkAnswer(){
+      let userAnswer = parseInt(document.getElementById("answer-box").value);
+      let calculateAnswer = calculateCorrectAnswer();
+      let isCorrect = userAnswer === calculateAnswer[0];
+
+      if (isCorrect){
+        alert("Hey! You got it right! :D");
+      }else{
+        alert(`awwww.... you answered ${userAnswer}. The correct answer was ${calculateAnswer[0]}!`);
+      }
+       
+      runGame(calculateAnswer[1]);
+}
+
+/**
+ * Gets the operands (the numbers) and the operator (plus,minus ect)
+ * directly from the DOM, and returns the correct answer.
+ */
+
+function calculateCorrectAnswer(){
+    let operand1 = pareInt(document.documentgetElementById("operand1").innerText);
+    let operand2 = pareInt(document.documentgetElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+}
+
+
+
+function incrementScore() {
+
+}
+
+
+
+function incrementWrongAnswer() {
+
+}
+
+
+
+function displayAdditionQuestion() {
+
+}
+
+
+
+function displaySubtractQuestion() {
+
+}
+
+
+
+function displayMultiplyQuestion() {
+
+}
+
+
+
+function displayMinusQuestion() {
+
 }
